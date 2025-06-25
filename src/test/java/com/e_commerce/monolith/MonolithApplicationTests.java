e10076c94ef279228b13c8425d227b76286db9f6
madhavan.ananthan <madhavan.ananthan@openweaver.com>
2025-06-25 16:54:21 +0530

    First commit

---
+++ src/test/java/com/e_commerce/monolith/MonolithApplicationTests.java
@@ -0,0 +1,13 @@
+package com.e_commerce.monolith;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonolithApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
